22b72298b7163eb3c8ca038173f03f45e1ea59c0
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-07 05:33:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version.
+//alert(5 + 6) // we are using nodejs , not browser
+
+
+console.log(3+
+    8) // code readability should be priority
+console.log("Lakshay")
+
+let name = "lakshay"
+let age= 43
+let isloggedIn = false 
+
+// number => 2 to power 53
+// bigint 
+
+//string => ""
+
+//boolean => true or false 
+
+//null => standalone value 
+//undefined => value not defined 
+
+//symbol => unique
+
+
+
+
+//object 
+
+
+
+
+console.log(typeof "Lakshay");
+console.log(typeof undefined);  //op - undefined 
+console.log(typeof null)  // op - object 
