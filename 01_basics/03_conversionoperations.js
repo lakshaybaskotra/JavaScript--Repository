5e01d8f755e955fb2b6d2102451d8e2535ab38ba
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-07 06:51:10 +0000

    different operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -25,5 +25,54 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 
 let exNum = 45
 let stringNum = String(exNum)
-console.log(stringNum);
-console.log( typeof stringNum);
+// console.log(stringNum);
+// console.log( typeof stringNum);
+
+
+// *** OPERATIONS ***//
+
+let value =3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(5-2);
+// console.log(2*50);
+// console.log(3**6);
+
+// console.log(4/9);
+// console.log(5%8);
+
+
+let str1 = "lakshay"
+let str2 = " baskotra"
+
+let str3 =str1 +str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%6);
+
+//console.log(+true);
+//console.log(true+); //error
+
+//console.log(+"");
+
+let num1 , num2 , num3
+
+num1=num2=num3= 4+5
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
