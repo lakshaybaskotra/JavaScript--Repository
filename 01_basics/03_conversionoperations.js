f63485dda32e4d7c306f62f0d062d4c89dab433f
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-07 05:54:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,29 @@
+// let score ="45fhfj" 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "45"=> 33
+// "45fjf"=> NaN
+// true => 1 ; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false 
+// ""=> false 
+// "lakshay"=>true
+
+
+let exNum = 45
+let stringNum = String(exNum)
+console.log(stringNum);
+console.log( typeof stringNum);
