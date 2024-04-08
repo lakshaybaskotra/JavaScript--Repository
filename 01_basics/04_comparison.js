ac02fdbcbb7f52915d4fb21331cb1c4e6737063b
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-08 17:59:49 +0000

    comparison in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>=7);
+// console.log(5<10);
+// console.log(5>7);
+// console.log(2==2);
+// console.log(2!=2);
+
+
+// console.log("5">2);
+// console.log("05">4);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined==0);
+console.log(undefined>= 0);
+console.log(undefined<=0);
+
+
+// === 
+
+console.log("67" === 67); 
