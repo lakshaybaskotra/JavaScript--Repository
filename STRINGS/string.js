68b4eb0cf4636d66d9db07b07d87d2150f7f14cb
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-14 23:01:05 +0530

    Add files via upload

---
+++ STRINGS/string.js
@@ -0,0 +1,27 @@
+const name="lakshay"
+const repocount = 12
+// console.log(name + repocount+ "  value");
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+const gamename = new String ('Akshay-goob')
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(4));
+// console.log(gamename.indexOf('g'));
+
+// const newString = gamename.substring(0,5)
+// console.log(newString);
+// const anotherstring = gamename.slice(-4,5)
+// console.log(anotherstring);
+
+// const newstr1= "  lakshay  "
+// console.log(newstr1);
+// console.log(newstr1.trim());
+
+const url = "https://lakshay100x%100devs.com"
+const replacedur = url.replace('%100', '-')
+console.log(replacedur);
+console.log(url.includes('lakshay'));
+console.log(gamename.split('-' ));
