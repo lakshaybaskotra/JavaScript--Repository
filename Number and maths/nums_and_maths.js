34365a9d28befaeb601a756c1410f3dbe96a9ecc
Lakshay  Baskotra <157511228+lakshaybaskotra@users.noreply.github.com>
2024-04-16 00:15:15 +0530

    Add files via upload

---
+++ Number and maths/nums_and_maths.js
@@ -0,0 +1,36 @@
+const score  = 79
+// console.log(score);
+const balance = new Number(567)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernum = 17.3456
+// console.log(othernum.toPrecision(2));
+
+
+const hundrd = 100000
+// console.log(hundrd.toLocaleString('en-IN'));
+
+/***********************MATHS************************************ */
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(7.4));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(5.4));
+// console.log(Math.sqrt(36));
+// console.log(Math.max(4,678,7890,345,67890,66,44444));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+
+const min =30
+const max = 79
+// console.log(Math.random()*(max-min+1))
+console.log(Math.floor((Math.random()*(max-min+1)) +min)
+
+
+
